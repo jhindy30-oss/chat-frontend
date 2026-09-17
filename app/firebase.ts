42ede31e279b283001ee7cf7db5f055185489577
jhindy30-oss <jhindy30@gmail.com>
2026-09-17 23:37:28 +0200

    Add Firebase initialization and Firestore setup

---
+++ app/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyuA8a7CPMAC8YKMXcJ5o1-7FDYBEjfcA",
+  authDomain: "sticker-responses.firebaseapp.com",
+  projectId: "sticker-responses",
+  storageBucket: "sticker-responses.firebasestorage.app",
+  messagingSenderId: "768304605509",
+  appId: "1:768304605509:web:030aca38010148f03af62c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
