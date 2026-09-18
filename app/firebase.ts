12e0644878c59511a695ac35fa400cf9960bb605
jhindy30-oss <jhindy30@gmail.com>
2026-09-18 22:24:11 +0200

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyuA8a7CPMAC8YKMXcJ5o1-7FDYBEjfcA",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
